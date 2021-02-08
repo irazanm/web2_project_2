686d6caace233bfa0a21d8a1dbfadc586eb14f1a
YaraAlthbiti <439201107@student.ksu.edu.sa>
2021-02-08 10:51:44 +0300

    ....

--- index.php
+++ index.php
@@ -10,8 +10,10 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+          
         <?php
         // put your code here
+        //....
         echo "<p>Hello</p>";
         ?>
     </body>
