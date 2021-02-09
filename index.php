c60b7610af43c85a8724ec3a88db6823ac8de46e
YaraAlthbiti <439201107@student.ksu.edu.sa>
2021-02-09 20:37:42 +0300

    new line

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ and open the template in the editor.
         //....
         echo "new code";
         echo "<p>Hello</p>";
+        echo "new de";
         echo "<p>Yara</p>";
         echo "<p> Lama</p>";
         ?>
