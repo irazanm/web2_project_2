39a7131f8cce4699970f503e288e5e21def2f486
irazanm <irazanjamil@gmail.com>
2021-02-12 03:40:58 +0300

    home page code

---
+++ javaS/home.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function Redirect_Login() {
+    var TypeOfUser = document.querySelector('input[name="Type"]:checked').value;
+    if (TypeOfUser == "Coach") {
+        location.replace("Coach_home.html");
+    } else {
+        location.replace("Trainee_home.html");
+    }
+}
+function Redirect_Signup() {
+    location.replace("Trainee_home.html");
+}
+
+
+
