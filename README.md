914834b6eb9e05244d2043ec34f4d98b8f074703
irazanm <irazanjamil@gmail.com>
2021-02-14 18:50:55 +0300

    Trainee page - footer problem

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # project web 2
 - Razan --> home page   
 - Shatha --> trainee home page  
-- Yara --> class info page  
-- Lama -->coach page  
+- Yara --> coach page  
+- Lama --> class info page  
--- javaS/home.js
+++ javaS/home.js
@@ -3,17 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+class user {
+
+    constructor(email = "", password = "", TypeOfUser = "") {
+        this.email = email;
+        this.password = password;
+        this.TypeOfUser = TypeOfUser;
+    }
+    getTypeOfUser() {
+        return this.TypeOfUser;
+    }
+
+}
+
 function Redirect_Login() {
-    var TypeOfUser = document.querySelector('input[name="Type"]:checked').value;
-    if (TypeOfUser == "Coach") {
+    var email = document.getElementById("login-email").value + "";
+    var password = document.getElementById("login-password").value + "";
+    var TypeOfUser = document.querySelector('input[name="Type"]:checked').value + "";
+
+    var person = new user(email, password, TypeOfUser);
+
+    if (person.getTypeOfUser() == "Coach") {
         location.replace("Coach_home.html");
     } else {
-        location.replace("Trainee_home.html");
+        Redirect_Signup();
     }
 }
 function Redirect_Signup() {
+   
     location.replace("Trainee_home.html");
 }
-
-
-
