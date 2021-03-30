554dc32515c39b522f4f47d2f60dec7cb909355a
YaraAlthbiti <439201107@student.ksu.edu.sa>
2021-03-30 22:28:48 +0300

    List php

---
+++ GetTraineeList.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require 'configration.php';
+if (isset($_POST['Show'])) {
+    $sql_trainees = "SELECT * FROM `trainee` WHERE id IN (SELECT trainee_id FROM enrolment WHERE class_id = " . $_POST['Show'] . ");";
+    $result_trainees = mysqli_query($connection, $sql_trainees);
+    if (!$result_trainees) {
+        echo '<script type="text/JavaScript"> window.alert("Something want wrong!! \n' . mysql_error($connection) . '"); </script>';
+    }
+     while($row = mysqli_fetch_assoc($result_trainees)){
+            $myObj[]=$row;
+        }
+        print_r(json_encode($myObj));
+        header('Content-Type: text/plain');   
+    }
+?>
