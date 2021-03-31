3cfdf26e034c56a8b6f6c0237fca7694849c53c1
YaraAlthbiti <439201107@student.ksu.edu.sa>
2021-03-31 14:14:54 +0300

    Get Trainee List

--- GetTraineeList.php
+++ GetTraineeList.php
@@ -7,15 +7,22 @@
  */
 require 'configration.php';
 if (isset($_POST['Show'])) {
-    $sql_trainees = "SELECT * FROM `trainee` WHERE id IN (SELECT trainee_id FROM enrolment WHERE class_id = " . $_POST['Show'] . ");";
-    $result_trainees = mysqli_query($connection, $sql_trainees);
-    if (!$result_trainees) {
-        echo '<script type="text/JavaScript"> window.alert("Something want wrong!! \n' . mysql_error($connection) . '"); </script>';
-    }
-     while($row = mysqli_fetch_assoc($result_trainees)){
-            $myObj[]=$row;
+        $sql_trainees = "SELECT * FROM `trainee` WHERE id IN (SELECT trainee_id FROM enrolment WHERE class_id = " . $_POST['Show'] . ");";
+        $result_trainees = mysqli_query($connection, $sql_trainees);
+        if (!$result_trainees) {
+            echo '<script type="text/JavaScript"> window.alert("Something want wrong!! \n' . mysql_error($connection) . '"); </script>';
         }
-        print_r(json_encode($myObj));
-        header('Content-Type: text/plain');   
+        $count = 0;
+        while($row = mysqli_fetch_assoc($result_trainees)){
+                $myObj[]=$row;
+                $count++;
+            }
+        if($count == 0){
+            print_r("0");
+            header('Content-Type: text/plain');
+        }else{
+            print_r(json_encode($myObj));
+            header('Content-Type: text/plain');
+        }
     }
 ?>
